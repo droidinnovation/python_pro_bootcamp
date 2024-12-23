5f2788b642bdb7811b1d1f1604284f1c6787ff4d
Quang Nguyen <droidinnovation88@gmail.com>
2024-12-23 11:00:02 +0700

    day21 create scoreboard and update score

--- snake_game_day21/main.py
+++ snake_game_day21/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score = Scoreboard()
 
 
 screen.listen()
@@ -28,6 +30,8 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        score.increase_score()
+
 
 screen.exitonclick()
 
---
+++ snake_game_day21/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT=('courier', 24, 'normal')
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+
+
+
+

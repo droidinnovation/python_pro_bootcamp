0a76884ef0c7723ab1f62c3636860495325f9ce4
Quang Nguyen <droidinnovation88@gmail.com>
2025-01-01 18:24:13 +0700

    Create cars manager

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -1,12 +1,14 @@
 from turtle import Screen
 import time
 from player import Player
+from car_manager import CarManager
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.onkey(player.go_up,"Up")
 screen.listen()
@@ -15,4 +17,7 @@ is_on_game = True
 while is_on_game:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
 

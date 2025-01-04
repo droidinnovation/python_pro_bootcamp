ca7a4c38f3634709203423758e4a0ddf7ce47a11
Quang Nguyen <droidinnovation88@gmail.com>
2025-01-04 16:46:54 +0700

    Read/Write high score to file Snake-Game

--- snake_game_day21/main.py
+++ snake_game_day21/main.py
@@ -35,30 +35,15 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]: #Use Slicing python start from 1
        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
-    # for segment in snake.segments:
-    #     if segment == snake.head:
-    #         pass
-    #     elif snake.head.distance(segment) < 10:
-    #         game_is_on = False
-    #         score.game_over()
-
-
-
-    # for segment in snake.segments:
-    #     if segment == snake.head:
-    #         pass
-    #     elif snake.head.distance(segment) < 10:
-    #         game_is_on = False
-    #         score.game_over()
 
 screen.exitonclick()
 
--- snake_game_day21/scoreboard.py
+++ snake_game_day21/scoreboard.py
@@ -3,6 +3,25 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT=('courier', 24, 'normal')
 
+
+def get_high_score():
+    try:
+        with open("data.txt", "r") as file:
+            high_score = int(file.read().strip())
+    except Exception as e:
+        print(f"Error reading from file: {e}")
+        high_score = 0
+    return high_score
+
+
+def save_high_score(score):
+     try:
+         with open("data.txt", "w") as file:
+             file.write(f"{score}")
+     except Exception as e:
+         print(f"Error reading from file: {e}")
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -12,21 +31,26 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 270)
+        self.update_score()
 
+    def reset(self):
+        if self.score > get_high_score():
+            # self.high_score = self.score
+            save_high_score(self.score)
+        self.score = 0
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score : {self.score}   High Score: {get_high_score()}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
-
 
 
 

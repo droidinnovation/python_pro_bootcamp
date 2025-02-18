69c6bd7c020bd0e790495be6d27980848fbcc873
Quang Nguyen <droidinnovation88@gmail.com>
2025-02-18 11:19:35 +0700

    modify entry generate password

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,5 +1,33 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8,10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2,4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2,4))]
+
+    password_lists = password_letters + password_numbers + password_symbols
+    shuffle(password_lists)
+
+    password = "".join(password_lists)
+    # Clear entry password before insert new text
+    password_entry.delete(0,END)
+
+    password_entry.insert(0, password)
+    print(password_lists)
+    print(password_entry.get())
+
+
+
 
 # ------- save data ---------
 def save_data():
@@ -18,9 +46,6 @@ def save_data():
                 data_file.write(f"{website} | {email} | {password} \n")
             website_entry.delete(0, END)
             password_entry.delete(0, END)
-
-
-
 
 
 # ------- UI Setup ---------
@@ -51,7 +76,7 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=35, command=save_data)
 add_button.grid(row=4, column=1, columnspan= 2)

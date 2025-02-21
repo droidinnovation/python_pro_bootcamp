74a93c45e3adccd0ccc5aa9079dd59b4a5364504
Quang Nguyen <droidinnovation88@gmail.com>
2025-02-21 13:57:50 +0700

    catch exception input data not alphabet

--- NATO-alphabet-proj/main.py
+++ NATO-alphabet-proj/main.py
@@ -2,16 +2,19 @@ import pandas as pd
 
 #TODO 1. Create a dictionary in this format:
 df_phonetic = pd.read_csv("nato_phonetic_alphabet.csv")
-
 alphabet_dict = {row.letter: row.code for (index, row) in df_phonetic.iterrows()}
 # print(alphabet_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    name_input = input("Input a word: ").strip().upper()
+    if len(name_input) > 0:
+        try:
+            output = [alphabet_dict[letter] for letter in name_input]
+        except KeyError:
+            print("Sorry only input alphabet please")
+            generate_phonetic()
+        else:
+            print(output)
 
-name_input = input("Input a word :").strip().upper()
-if len(name_input)>0:
-    # alphabet_list = list(name_input.strip())
-    # result = [value for (key, value) in alphabet_dict.items() if key in alphabet_list]
-    result = [alphabet_dict[letter] for letter in name_input]
-
-    print(result)
+generate_phonetic()

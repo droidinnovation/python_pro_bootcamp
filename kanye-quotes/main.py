09f606b8bd7b13c28140415d26ac97cdfa92800c
Quang Nguyen <droidinnovation88@gmail.com>
2025-04-05 16:19:06 +0700

    Kanye-quote app

---
+++ kanye-quotes/main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import requests
+
+def get_quote():
+#     1. Make a get() request to the Kanye Rest Api
+#     2. Raise an exception if the request returned an unsuccessful status code
+# 3. Parse the JSON to obtain the quote text.
+# 4. Display the quote in the canvas 'quote_text' widget
+    response = requests.get("https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+    # print(response)
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150,207,image=background_img)
+quote_text = canvas.create_text(150,207,text="Kanye Quote Goes Here", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_btn = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_btn.grid(row=1,column=0)
+
+get_quote()
+
+window.mainloop()
